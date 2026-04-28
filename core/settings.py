c54e61832e93eb6b46f428762107b2979f2e9fb1
Chhotala Ravi <chhotalaravi24@gmail.com>
2026-04-28 14:53:37 +0530

    fix static files with whitenoise

--- core/settings.py
+++ core/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -143,3 +144,5 @@ DEFAULT_FROM_EMAIL = 'Veloura <ravichhotala24@gmail.com>'
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
